778988e9b80e5d7942494d5c6ef6e7c05b4f0d8f
Diana Nguyen <drex.nguyen@gmail.com>
2026-09-17 16:26:17 -0500

    Added flask template

--- src/server.py
+++ src/server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
+names = []
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/catch", methods=["POST"])
+def catch():
+    global names 
+    if request.form.get("name"):
+        names.append(request.form.get("name"))
+    return render_template("index.html", names=names)
